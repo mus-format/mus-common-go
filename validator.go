b06e56211ca32cb4376a43212b8361e04950880f
yra <ymz.ncnk@outlook.com>
2023-04-26 16:45:51 +0300

    Correct comment

--- validator.go
+++ validator.go
@@ -1,11 +1,13 @@
 package muscom
 
-// Validator performs data validation.
+// Validator is the interface that wraps Validate method.
+//
+// Validate performs data validation.
 type Validator[T any] interface {
 	Validate(t T) error
 }
 
-// ValidatorFn is a function implementation of the Validator interface.
+// ValidatorFn is a functional implementation of the Validator interface.
 type ValidatorFn[T any] func(t T) (err error)
 
 func (fn ValidatorFn[T]) Validate(t T) (err error) {
